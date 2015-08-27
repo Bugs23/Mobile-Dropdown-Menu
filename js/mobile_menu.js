f4521f7a1b0b8fdf14b05bd1720a3d6535cef25b
Raymond <raymondurrutia2@yahoo.com>
2015-08-27 16:50:59 -0400

    made some minor changes to the css

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,25 +1,25 @@
-// Create a select and append to #menu
+// create a select element and append to #menu
 var $select = $("<select></select>");
 $("#menu").append($select);
 
-// Cycle over menu links
+// cycle over menu links
 $("#menu a").each(function(){
   var $anchor = $(this);
-  // Create an option
+  // create an option
   var $option = $("<option></option>");  
-  // Deal with selected options depending on current page
+  // deal with selected options depending on current page
   if ($anchor.parent().hasClass("selected")) {
     $option.prop("selected", true);
   };
-  // Set option's value to href
+  // set option's value to href
   $option.val($anchor.attr("href"));
-  // Option's text is the text of the link
+  // option's text is the text of the link
   $option.text($anchor.text());
-  // Append option to select
+  // append option to select
   $select.append($option);
 })
-// Bind change listener to the select
+// bind change listener to the select
 $select.change(function(){
-  // Go to "select" location
+  // go to "select" location
   window.location = $select.val();
 });
